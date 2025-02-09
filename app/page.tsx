1f8db6a79af1ecdc06b85c28b42108f417a4bc4f
sabbir-shuvo-ux <shuvosabbir134@gmail.com>
2025-02-09 15:42:21 +0600

    initiallized next-auth

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,12 @@
 import { Button } from "@/components/ui/button";
+import { auth } from "@/lib/auth";
 
-const HomePage = () => {
+const HomePage = async () => {
+  const session = await auth();
+  console.log(session);
   return (
     <div>
+      <div className="">hello, {session?.user?.email}</div>
       <Button>Hello</Button>
     </div>
   );
